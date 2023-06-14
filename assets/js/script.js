3344653181e2e1742a090e77f36442a5b521d706
Marcelo Frederico Slabcouski Branco Garcia <78546315+slabcouski@users.noreply.github.com>
2023-06-14 19:28:43 +0000

    16:28 14/06/23

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,35 @@ $(document).ready(function(){
     $('#telefone').mask('(00) 00000-0000');
     $('#cpf').mask('000.000.000-00', {reverse: true});
     $('#cep').mask('00000-000');
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereço: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            email: 'Por favor, insira seu e-mail',
+            telefone: 'Por favor, insira seu telefone',
+            cpf: 'Por favor, insira seu cpf',
+            endereço: 'Por favor, insira seu endereço',
+            cep: 'Por favor, insira seu cep', 
+        }
+    })
 })
